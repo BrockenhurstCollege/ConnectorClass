fb900de90625928bf8ac22c74ac9581e85ca1b2e
brockComputing <cbrown@brock.ac.uk>
2017-09-06 10:46:56 +0100

    Added Class

---
+++ clsDBConnector.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Data.OleDb;
+
+namespace StudentLatesCSV1
+{
+    class clsDBConnector
+    {
+        OleDbConnection conn = new OleDbConnection();
+        string dbProvider;
+        string dbSource;
+
+        public void Connect()
+        {
+           // dbProvider = "Provider=Microsoft.ACE.OLEDB.12.0;";
+           // dbSource = "Data Source =dbStudentlates.accdb";
+           // conn.ConnectionString = dbProvider + dbSource;
+            conn.ConnectionString =  Properties.Settings.Default.dbStudentlatesConnectionString ;
+            conn.Open();
+        }
+
+        public void close()
+        {
+            conn.Close();
+        }
+
+        public void DoDML(string dmlString)
+        {
+            OleDbCommand cmd;
+            cmd = new OleDbCommand(dmlString, conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public OleDbDataReader DoSQL(string sqlString)
+        {
+            OleDbCommand cmd;
+            cmd = new OleDbCommand(sqlString, conn);
+            return cmd.ExecuteReader() ;
+        }  
+    }
+}
